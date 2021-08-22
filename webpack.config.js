6964f5baf374f5c8111bc8e28a7b7c3cc955d3ac
ak-yaps <contact@ayaba-kumako.com>
2021-08-22 21:06:53 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+let config = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "./dist/main.js"
+  },
+  module: {
+    rules: [{
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: "defaults"
+              }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[hash][ext]'
+        }
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'test RAJA',
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ],
+  mode: 'development',
+  output: {
+    clean: true
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true
+  }
+};
+
+module.exports = config;
